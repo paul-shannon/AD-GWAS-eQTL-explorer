64aed173398aef947ab803c2ce039aa746955c18
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-11-19 18:24:43 -0800

    gwas catalog now supported

--- R/ADvariantExplorer.R
+++ R/ADvariantExplorer.R
@@ -2,7 +2,8 @@
 #' @description A template for building documented, tested R6 classes
 #' @name ADvariantExplorer
 
-#' @field id identifier for a class object
+#' @import catalogueR
+#' @import gwascat
 #'
 #' @examples
 #'   rt <- R6Template$new(id="abc")
@@ -11,24 +12,82 @@
 ADvariantExplorer = R6Class("ADvariantExplorer",
 
     #--------------------------------------------------------------------------------
-    private = list(id=NULL
+    private = list(targetGene=NULL,
+                   loc.chrom=NULL,
+                   loc.start=NULL,
+                   loc.end=NULL,
+                   tbl.gwascat=NULL,
+                   tbl.gwascat.filtered=NULL,
+                   tag.snps=NULL,
+                   tbl.haplotypes=NULL
                    ),
 
     #--------------------------------------------------------------------------------
     public = list(
          #' @description
          #' Creates a new instance of this [R6][R6::R6Class] class.
-         #' @param id character, an indentifier for this object
+         #' @param targetGene character, a candidate gene for this AD GWAS locus
+         #' @param loc.chrom character, the chromosome of the region to consider
+         #' @param loc.start numeric, the start of the region to consider
+         #' @param loc.end   numeric, the end of the region to consider
          #' @return a new instance of ADvariantExplorer
-        initialize = function(id){
-            private$id <- id
+        initialize = function(targetGene, loc.chrom, loc.start, loc.end){
+            private$targetGene <- targetGene
+            private$loc.chrom <- loc.chrom
+            private$loc.start <- loc.start
+            private$loc.end   <- loc.end
+            f <- system.file(package="ADvariantExplorer", "extdata", "gwascat-31oct2021.RData")
+            gr.gwascat <- get(load(f))
+            tbl.gwascat <- as.data.frame(gr.gwascat)
+            colnames(tbl.gwascat)[1] <- "chrom"
+            tbl.gwascat$chrom <- as.character(tbl.gwascat$chrom)
+            private$tbl.gwascat <- tbl.gwascat
             },
+
         #------------------------------------------------------------
-        #' @description accessor for the object's id field
-        #' @return the current value of the id member
-        getID = function(){
-            private$id
+        #' @description accessor for the object's targetGene field
+        #' @return the current value of the targetGene member
+        getTargetGene = function(){
+            private$targetGene
+        },
+
+        #------------------------------------------------------------
+        #' @description access to the EMBL-EBI GWAS catalog via bioc, filtered
+        #'  by location and/or targetGene
+        #' @return a data.frame
+        getFilteredGwasTable = function(){
+            tbl.sub <- subset(private$tbl.gwascat, chrom==sub("chr", "", private$loc.chrom) &
+                                                   start >= private$loc.start & end <= private$loc.end |
+                              grepl("CLU", MAPPED_GENE))
+            soi <- grep("Alzheimer", tbl.sub$STUDY, ignore.case=TRUE)
+            tbl <- tbl.sub[soi,]
+            coi <- c("SNPS", "P.VALUE", "MAPPED_GENE", "FIRST.AUTHOR", "DATE", "INITIAL.SAMPLE.SIZE", "STUDY" )
+            tbl <- tbl[, coi]
+            tbl$STUDY <- substr(tbl$STUDY, 1, 30)
+            tbl$INITIAL.SAMPLE.SIZE <- substr(tbl$INITIAL.SAMPLE.SIZE, 1, 20)
+            new.order <- order(tbl$P.VALUE, decreasing=FALSE)
+            tbl <- tbl[new.order,]
+            rownames(tbl) <- NULL
+            private$tbl.gwascat.filtered
+            tbl
+            },
+
+        #------------------------------------------------------------
+        #' @description access to the complete EMBL-EBI GWAS catalog via bioc
+        #' @param trim.columns logical reduce to a legible 7 columns
+        #' @return a data.frame
+        getFullGwasTable = function(trim.columns=FALSE){
+            tbl.full <- private$tbl.gwascat
+            if(!trim.columns)
+                invisible(tbl.full)
+            coi <- c("SNPS", "P.VALUE", "MAPPED_GENE", "FIRST.AUTHOR", "DATE", "INITIAL.SAMPLE.SIZE", "STUDY" )
+            tbl <- tbl.full[, coi]
+            tbl$STUDY <- substr(tbl$STUDY, 1, 30)
+            tbl$INITIAL.SAMPLE.SIZE <- substr(tbl$INITIAL.SAMPLE.SIZE, 1, 20)
+            invisible(tbl)
             }
+
+
        ) # public
 
     ) # class
--- inst/unitTests/test_ADvariantExplorer.R
+++ inst/unitTests/test_ADvariantExplorer.R
@@ -4,6 +4,7 @@ library(ADvariantExplorer)
 runTests <- function()
 {
     test_ctor()
+    test_gwasCatalog()
 
 } # runTests
 #----------------------------------------------------------------------------------------------------
@@ -11,12 +12,32 @@ test_ctor <- function()
 {
     message(sprintf("--- test_ctor"))
 
-    id <- "abc"
-    rt <- ADvariantExplorer$new(id)
-    checkTrue(all(c("R6", "ADvariantExplorer") %in% class(rt)))
-    checkEquals(rt$getID(), id)
+    targetGene <- "CLU"
+    loc.chrom <- "chr8"
+    loc.start <- 27447528
+    loc.end   <- 27764088
+    avx <- ADvariantExplorer$new(targetGene, loc.chrom, loc.start, loc.end)
+    checkTrue(all(c("R6", "ADvariantExplorer") %in% class(avx)))
+    checkEquals(avx$getTargetGene(), targetGene)
 
 } # test_ctor
+#----------------------------------------------------------------------------------------------------
+test_gwasCatalog <- function()
+{
+    message(sprintf("--- test_ctor"))
+
+    targetGene <- "CLU"
+    loc.chrom <- "chr8"
+    loc.start <- 27447528
+    loc.end   <- 27764088
+    avx <- ADvariantExplorer$new(targetGene, loc.chrom, loc.start, loc.end)
+    tbl.f <- avx$getFilteredGwasTable()
+    checkTrue(nrow(tbl.f) < 30)
+    tbl.all <- avx$getFullGwasTable(trim.columns=TRUE)
+    dim(tbl.all)
+    checkTrue(nrow(tbl.all) > 110000)
+
+} # test_gwasCatalog
 #----------------------------------------------------------------------------------------------------
 if(!interactive())
     runTests()
